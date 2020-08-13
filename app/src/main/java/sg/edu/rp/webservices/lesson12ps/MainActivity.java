e7e0986a1140ce4c06778f8d04948185dd426746
18016375 <cecilialui16@yahoo.com.sg>
2020-08-13 21:47:47 +0800

    activities 1-100000000

--- app/src/main/java/sg/edu/rp/webservices/lesson12ps/MainActivity.java
+++ app/src/main/java/sg/edu/rp/webservices/lesson12ps/MainActivity.java
@@ -1,14 +1,68 @@
 package sg.edu.rp.webservices.lesson12ps;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    int notifReqCode = 1234;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void onReceive(Context context, Intent i) {
+
+        int id = i.getIntExtra("id", -1);
+        String name = i.getStringExtra("name");
+        String desc = i.getStringExtra("desc");
+
+        Tasks task = (Tasks) i.getSerializableExtra("task");
+
+        NotificationManager notificationManager = (NotificationManager)
+                context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new
+                    NotificationChannel("default", "Default Channel",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+
+            channel.setDescription("This is for default notification");
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pIntent = PendingIntent.getActivity(context, notifReqCode,
+                intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        NotificationCompat.Action action = new
+                NotificationCompat.Action.Builder(
+                R.mipmap.ic_launcher,
+                desc,
+                pIntent).build();
+
+        NotificationCompat.WearableExtender extender = new
+                NotificationCompat.WearableExtender();
+        extender.addAction(action);
+
+
+        // build notification
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "default");
+        builder.setContentTitle("Task Manager Reminder");
+        builder.setContentText(name);
+        builder.setSmallIcon(android.R.drawable.ic_dialog_info);
+        builder.setContentIntent(pIntent);
+        builder.setAutoCancel(true);
+
+        builder.extend(extender);
+
+        Notification n = builder.build();
+
+        notificationManager.notify(notifReqCode, n);
     }
 }
---
+++ app/src/main/java/sg/edu/rp/webservices/lesson12ps/Tasks.java
@@ -0,0 +1,4 @@
+package sg.edu.rp.webservices.lesson12ps;
+
+public class Tasks {
+}
